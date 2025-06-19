6a9894e348473cebea7a3481e28b56ea3e962dfb
gnunoi <gnunoi@163.com>
2025-06-19 10:42:43 +0800

    update

--- WiseMath/views.py
+++ WiseMath/views.py
@@ -83,14 +83,47 @@ def test_html(request):
         print(request)
         return render(request,'test.html', locals())
 
+from django.shortcuts import render
 def test_html2(request):
-    from django.shortcuts import render
+    name_list = [
+        '24点计算',  # 0
+        '乘法速算',  # 1
+        '四则运算',  # 2
+        '质因数分解',  # 3
+        '单位换算',  # 4
+        '分数计算',  # 5
+        '小数计算',  # 6
+        '比例计算',  # 7
+        '周长计算',  # 8
+        '面积计算',  # 9
+        '体积计算',  # 10
+        '倒数之和',  # 11
+        '乘幂运算',  # 12
+        '一元一次方程',  # 13
+        '二元一次方程组',  # 14
+        '一元二次方程',  # 15
+    ]
+
+    options = [{'index': i, 'value': name} for i, name in enumerate(name_list)]
+
     context = {
         'option_list': list(range(16)),
+        'options': options,  # 将包含索引和值的列表传递给模板
     }
+
+    selected_index = 0  # 默认值
+
     if request.method == 'POST':
-        selected_option = request.POST.get('selected_option', '未选择')
-        context = {**context,
-            'selected_option': selected_option
-        }
+        selected_index = int(request.POST.get('selected_option', 0))
+        # 将所选选项的索引存储到会话中
+        request.session['selected_option_index'] = selected_index
+    else:
+        # 从会话中获取所选选项的索引，默认为 0
+        selected_index = request.session.get('selected_option_index', 0)
+
+    # 获取对应的字符串名称
+    selected_option = name_list[selected_index]
+
+    context = {**context, 'selected_option': selected_option, 'selected_index': selected_index}
+
     return render(request, 'test_html2.html', context)
